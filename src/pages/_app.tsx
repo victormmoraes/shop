5797831cef50c597e5bbd0dd6bce304b462ec64f
Victor Medrado <dev.medrado@gmail.com>
2023-04-05 11:32:49 -0300

    Header da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
-globalStyles();
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
