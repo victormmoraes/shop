250eca5d2ffeb5b30e54a9b2cf445c47fb40e719
Victor Medrado <dev.medrado@gmail.com>
2023-04-05 11:46:53 -0300

    Trabalhando com o componente Image do next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
-
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
