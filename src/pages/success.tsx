5c70b456cc09e44fe0dcc1a96cacd0508ddb0be5
Victor Medrado <dev.medrado@gmail.com>
2023-04-27 23:53:41 -0300

    Criando página de sucesso para prepara-la para receber os dados pós compra

---
+++ src/pages/success.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
+export default function Success() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>Uhuul <strong>Victor</strong>, sua <strong>Camiseta X</strong> já está a caminho de sua casa!</p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
+}
